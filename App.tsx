420110e3cae35b065873b45e860668e00703c862
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2026-05-04 20:33:13 -0300

    separando estilizacao da estrutura do app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './styles';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oi Fernando Tozetto!</Text>
+      <Text style={styles.text}>
+        Oi Fernando Tozetto!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 700,
+    color: 'blue',
+  },
+});
