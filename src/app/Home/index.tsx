2a5c7e276ad1a8c76cefba465d649c9c0771ec9d
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2026-05-06 20:02:20 -0300

    adicionando paths

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View, Image } from 'react-native';
+import { View, Image } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require('../../assets/logo.png')} style={styles.logo} />
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
     </View>
   );
 }
