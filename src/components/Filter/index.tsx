dbbf7a8409f948d6a5d79d05666bd25fedd82feb
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2026-05-17 19:45:50 -0300

    trabalhando com icones

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,4 +1,5 @@
 import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { CircleCheck } from "lucide-react-native"
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
@@ -14,6 +15,8 @@ export function Filter({ status, isActive, ...rest }: Props) {
         style={[styles.container, {opacity: isActive ? 1 : 0.5}]} 
         activeOpacity={0.8}
         {...rest}>
+
+        <CircleCheck size={18} color="#000"/>
             <Text style={styles.title}>
                 { status === FilterStatus.DONE ? 'Comprados' : 'Pendentes' }
             </Text>
